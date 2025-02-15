b77a6d1ca98a4364ba5cefa856b5f7989ad10aef
felix <felixmoeran@gmail.com>
2025-02-15 18:32:00 +0000

    .

---
+++ problemsets/subtractMinSort.cpp
@@ -0,0 +1,149 @@
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+
+using namespace std;
+
+#define ll long long
+#define mn(x) const ll N = x
+#define ito(n) for(int i=0; i<n; i++)
+#define jto(n) for(int j=0; j<n; j++)
+#define loop(n)
+#define all(v) v.begin(), v.end()
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+mn(2e5+1);
+int n, a[N];
+
+void solve() {
+    cin >> n;
+    ito(n) cin >> a[i];
+
+    ito(n-1) {
+        if (a[i] > a[i+1]) {
+            NO;
+            return;
+        }
+        a[i+1] -= a[i];
+    }
+    YES;
+
+}
+
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t=1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
+
+
+
+
+
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+
+using namespace std;
+
+#define ll long long
+#define mn(x) const ll N = x
+#define ito(n) for(int i=0; i<n; i++)
+#define jto(n) for(int j=0; j<n; j++)
+#define loop(n)
+#define all(v) v.begin(), v.end()
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+mn(2e5+1);
+int n, a[N];
+
+void solve() {
+    cin >> n;
+    ito(n) cin >> a[i];
+
+    ito(n-1) {
+        if (a[i] > a[i+1]) {
+            NO;
+            return;
+        }
+        a[i+1] -= a[i];
+    }
+    YES;
+
+}
+
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t=1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
